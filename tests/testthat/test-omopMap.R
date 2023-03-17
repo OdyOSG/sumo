153b3c6af3277572b4ebf695adfcfaca62f5f1cb
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-03-17 13:13:15 -0400

    add test that use cdm

---
+++ tests/testthat/test-omopMap.R
@@ -0,0 +1,26 @@
+test_that("search omop works", {
+  conceptIds <- c("439777")
+  keyword_tst <- searchOmop(conceptIds = conceptIds, cdm = cdm, explosion = TRUE)
+  expect_type(keyword_tst, "character")
+  expect_match(keyword_tst, "MeSH")
+  expect_match(keyword_tst, "Anemia")
+})
+
+
+
+test_that("make dict works", {
+  resDict <- makeDict(res, cdm)
+  expect_s3_class(resDict, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(ncol(resDict), 5)
+  expect_type(resDict$concept_id_1, "integer")
+})
+
+
+test_that("add dictionary back to res", {
+  resDict <- makeDict(res, cdm)
+  res <- addDictToRes(res, resDict)
+  expect_s3_class(res, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(ncol(res), 13)
+  expect_type(res$concepts, "character")
+})
+
